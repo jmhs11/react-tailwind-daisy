90d71c50cd35a286031040418a2615caacd27ec2
jmhs11 <josemariahs@gmail.com>
2022-08-08 21:04:26 +0200

    Add DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 const App = () => {
-	return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+	return (
+		<>
+			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<button className='btn btn-primary'>Button</button>
+		</>
+	);
 };
 
 export default App;
