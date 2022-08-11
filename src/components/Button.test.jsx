6f3fad7bd3061324138e688e152a13235205680f
jmhs11 <josemariahs@gmail.com>
2022-08-11 17:26:27 +0200

    Update test

--- src/components/Button.test.jsx
+++ src/components/Button.test.jsx
@@ -6,19 +6,23 @@ describe('Button', () => {
 	const buttonText = 'Button from test';
 
 	it('should render button correctly', () => {
+		// Arrange
 		const component = render(<Button text={buttonText} />);
+		// Act
+		// Assert
 		component.getByText(buttonText);
 	});
 
 	it('clicking the button calls event handler once', () => {
+		// Arrange
 		const mockHandler = vi.fn();
-
 		const component = render(
 			<Button text={buttonText} handleClick={mockHandler} />
 		);
 		const button = component.getByText(buttonText);
+		// Act
 		fireEvent.click(button);
-
+		// Assert
 		expect(mockHandler).toHaveBeenCalledTimes(1);
 	});
 });
